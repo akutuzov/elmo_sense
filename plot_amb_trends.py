6925e6082e39603027eb4870340d4533f7d1908c
Andrey Kutuzov <akutuzov72@gmail.com>
2020-05-20 19:19:39 +0200

    Ambiguity plots from token embeddings

--- plot_amb_trends.py
+++ plot_amb_trends.py
@@ -4,6 +4,8 @@
 import argparse
 from elmo_helpers import *
 import pylab as plot
+from matplotlib.ticker import StrMethodFormatter
+from operator import itemgetter
 
 if __name__ == '__main__':
     logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
@@ -12,6 +14,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     arg = parser.add_argument
     arg('--input', '-i', help='Path to the directory with npz files', required=True)
+    arg('--model', '-m', help='Name of the model', default='Wikipedia')
+    arg('--outfile', '-o', help='Name of the output file')
     args = parser.parse_args()
 
     e_60 = np.load(os.path.join(args.input, '1960.npz'))
@@ -32,19 +36,77 @@ if __name__ == '__main__':
     coeffs = {}
     logger.info('Calculating diversities...')
     for word in words:
+        valid = True
+        for decade in [e_60, e_70, e_80, e_90, e_00]:
+            if decade[word].shape[0] < 2:
+                valid = False
+                break
+        if not valid:
+            print('%s too rare! Skipping.' % word)
+            continue
         coeffs[word] = []
         for decade in [e_60, e_70, e_80, e_90, e_00]:
             coeff = diversity(decade[word])
             coeffs[word].append(coeff)
     logger.info('Finished!')
 
-    for word in words:
+    for word in coeffs:
         plot.plot(int_years, coeffs[word], label=word)
-    plot_title = 'Changes in word ambiguity over time, %s' % args.input.split('/')[-1]
+    plot_title = 'Changes in word ambiguity over time, %s frequency' % args.input.split('/')[-1]
     plot.title(plot_title)
     plot.xticks(int_years)
     plot.xlabel('Decades')
     plot.ylabel('ELMo diversity coefficients')
-    plot.savefig('%s_elmo_dia_all.png' % args.input.split('/')[-1], dpi=300)
+    plot.savefig('%s_%s_elmo_dia_all.png' % (args.model, args.input.split('/')[-1]), dpi=300)
     plot.close()
     plot.clf()
+
+    changes = []
+
+    for word in coeffs:
+        for i in range(len(coeffs[word]) - 1):
+            diff = round(coeffs[word][i + 1] - coeffs[word][i], 4)
+            out = (word, (str(int_years[i]), str(int_years[i + 1])), diff)
+            changes.append(out)
+
+    sorted_changes = [d for d in sorted(changes, key=itemgetter(2), reverse=True)]
+    logger.info('Top 5 most changed:')
+    for i in sorted_changes[:5]:
+        logger.info(i)
+        # interesting_words.add(i[0])
+    logger.info('=============')
+    logger.info('Top 5 least changed:')
+    for i in sorted_changes[-5:]:
+        logger.info(i)
+        # interesting_words.add(i[0])
+    logger.info('=============')
+    year_changes = {}
+    for el in changes:
+        if el[1] not in year_changes:
+            year_changes[el[1]] = []
+        year_changes[el[1]].append(el[2])
+
+    for year in year_changes:
+        print(year, np.mean(year_changes[year]))
+    sorted_doubles = sorted(year_changes.keys())
+    double_labels = [el[0] + '-' + el[1] for el in sorted_doubles]
+
+    plot.clf()
+    plot.bar(range(len(sorted_doubles)), [np.mean(year_changes[year]) for year in sorted_doubles],
+             tick_label=double_labels, color='red')
+    plot_title = 'Average changes in lexical ambiguity per decade, %s frequency' \
+                 % args.input.split('/')[-1]
+    plot.title(plot_title)
+    plot.xticks()
+    plot.xlabel('Transitions between decades')
+    plot.ylabel('Change in the average ELMo variation coefficients')
+    plot.gca().yaxis.set_major_formatter(StrMethodFormatter('{x:,.4f}'))
+    plot.savefig('%s_%s_elmo_dia_years.png' % (args.model, args.input.split('/')[-1]), dpi=300)
+    plot.close()
+    plot.clf()
+
+    if args.outfile:
+        with open(args.outfile, 'a') as f:
+            for word in coeffs:
+                coeff = '\t'.join([str(d) for d in coeffs[word]])
+                f.write('\t'.join([word, coeff]) + '\n')
