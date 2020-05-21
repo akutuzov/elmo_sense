9d3fed6e2b15f3a92d28780e00b5d842d5ce4939
akutuzov <akutuzov72@gmail.com>
2020-05-21 14:55:27 +0200

    Ambiguity data for incremental COHA

--- plot_amb_trends.py
+++ plot_amb_trends.py
@@ -13,51 +13,66 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     arg = parser.add_argument
-    arg('--input', '-i', help='Path to the directory with npz files', required=True)
-    arg('--model', '-m', help='Name of the model', default='Wikipedia')
+    arg('--input', '-i', help='Path to the directory with npz files or to the csv', required=True)
+    arg('--model', '-m', help='Name of the model', default='wikipedia')
+    arg('--calc', '-c', help='Calculate from npz or tak from csv?', choices=['npz', 'csv'],
+        default='npz')
     arg('--outfile', '-o', help='Name of the output file')
     args = parser.parse_args()
 
-    e_60 = np.load(os.path.join(args.input, '1960.npz'))
-    logger.info('Loaded an array of %d entries from %s' % (len(e_60), '1960.npz'))
-    e_70 = np.load(os.path.join(args.input, '1970.npz'))
-    logger.info('Loaded an array of %d entries from %s' % (len(e_70), '1970.npz'))
-    e_80 = np.load(os.path.join(args.input, '1980.npz'))
-    logger.info('Loaded an array of %d entries from %s' % (len(e_80), '1980.npz'))
-    e_90 = np.load(os.path.join(args.input, '1990.npz'))
-    logger.info('Loaded an array of %d entries from %s' % (len(e_90), '1990.npz'))
-    e_00 = np.load(os.path.join(args.input, '2000.npz'))
-    logger.info('Loaded an array of %d entries from %s' % (len(e_00), '2000.npz'))
-
     int_years = [1960, 1970, 1980, 1990, 2000]
-
-    words = e_00.files
-
     coeffs = {}
-    logger.info('Calculating diversities...')
-    for word in words:
-        valid = True
-        for decade in [e_60, e_70, e_80, e_90, e_00]:
-            if decade[word].shape[0] < 2:
-                valid = False
-                break
-        if not valid:
-            print('%s too rare! Skipping.' % word)
-            continue
-        coeffs[word] = []
-        for decade in [e_60, e_70, e_80, e_90, e_00]:
-            coeff = diversity(decade[word])
-            coeffs[word].append(coeff)
-    logger.info('Finished!')
+
+    if args.calc == 'npz':
+
+        e_60 = np.load(os.path.join(args.input, '1960.npz'))
+        logger.info('Loaded an array of %d entries from %s' % (len(e_60), '1960.npz'))
+        e_70 = np.load(os.path.join(args.input, '1970.npz'))
+        logger.info('Loaded an array of %d entries from %s' % (len(e_70), '1970.npz'))
+        e_80 = np.load(os.path.join(args.input, '1980.npz'))
+        logger.info('Loaded an array of %d entries from %s' % (len(e_80), '1980.npz'))
+        e_90 = np.load(os.path.join(args.input, '1990.npz'))
+        logger.info('Loaded an array of %d entries from %s' % (len(e_90), '1990.npz'))
+        e_00 = np.load(os.path.join(args.input, '2000.npz'))
+        logger.info('Loaded an array of %d entries from %s' % (len(e_00), '2000.npz'))
+
+        words = e_00.files
+
+        logger.info('Calculating diversities...')
+        for word in words:
+            valid = True
+            for decade in [e_60, e_70, e_80, e_90, e_00]:
+                if decade[word].shape[0] < 2:
+                    valid = False
+                    break
+            if not valid:
+                print('%s too rare! Skipping.' % word)
+                continue
+            coeffs[word] = []
+            for decade in [e_60, e_70, e_80, e_90, e_00]:
+                coeff = diversity(decade[word])
+                coeffs[word].append(coeff)
+        logger.info('Finished!')
+
+    else:
+        logger.info('Loading diversities from %s...' % args.input)
+        with open(args.input, 'r') as f:
+            for line in f:
+                res = line.strip().split('\t')
+                word, w_60, w_70, w_80, w_90, w_00 = res
+                coeffs[word.strip()] = [float(d) for d in [w_60, w_70, w_80, w_90, w_00]]
 
     for word in coeffs:
         plot.plot(int_years, coeffs[word], label=word)
-    plot_title = 'Changes in word ambiguity over time, %s frequency' % args.input.split('/')[-1]
+    plot_title = 'Changes in word ambiguity over time, %s frequency' \
+                 % args.input.split('/')[-1].split('_')[2].split('.')[0]
     plot.title(plot_title)
     plot.xticks(int_years)
     plot.xlabel('Decades')
     plot.ylabel('ELMo diversity coefficients')
-    plot.savefig('%s_%s_elmo_dia_all.png' % (args.model, args.input.split('/')[-1]), dpi=300)
+    plot.savefig('%s_%s_elmo_dia_all.png' % (args.model,
+                                             args.input.split('/')[-1].split('_')[2].
+                                             split('.')[0]), dpi=300)
     plot.close()
     plot.clf()
 
@@ -70,12 +85,12 @@ if __name__ == '__main__':
             changes.append(out)
 
     sorted_changes = [d for d in sorted(changes, key=itemgetter(2), reverse=True)]
-    logger.info('Top 5 most changed:')
+    logger.info('Top 5 with increased ambiguity:')
     for i in sorted_changes[:5]:
         logger.info(i)
         # interesting_words.add(i[0])
     logger.info('=============')
-    logger.info('Top 5 least changed:')
+    logger.info('Top 5 with decreased ambiguity:')
     for i in sorted_changes[-5:]:
         logger.info(i)
         # interesting_words.add(i[0])
@@ -95,13 +110,15 @@ if __name__ == '__main__':
     plot.bar(range(len(sorted_doubles)), [np.mean(year_changes[year]) for year in sorted_doubles],
              tick_label=double_labels, color='red')
     plot_title = 'Average changes in lexical ambiguity per decade, %s frequency' \
-                 % args.input.split('/')[-1]
+                 % args.input.split('/')[-1].split('_')[2].split('.')[0]
     plot.title(plot_title)
     plot.xticks()
     plot.xlabel('Transitions between decades')
     plot.ylabel('Change in the average ELMo variation coefficients')
     plot.gca().yaxis.set_major_formatter(StrMethodFormatter('{x:,.4f}'))
-    plot.savefig('%s_%s_elmo_dia_years.png' % (args.model, args.input.split('/')[-1]), dpi=300)
+    plot.savefig('%s_%s_elmo_dia_years.png' % (args.model,
+                                               args.input.split('/')[-1].split('_')[2].
+                                               split('.')[0]), dpi=300)
     plot.close()
     plot.clf()
 
