e115e42b921230af024211a1b2bfd5b103168883
Andrey Kutuzov <akutuzov72@gmail.com>
2020-05-25 18:43:10 +0200

    Simultaneous plots

--- vis_proj_two.py
+++ vis_proj_two.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-from docopt import docopt
+import argparse
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 
@@ -11,24 +11,21 @@ def main():
     Produce PCA plot of a word's contextualised embeddings.
     """
 
-    # Get the arguments
-    args = docopt("""Produce PCA plot of a word's contextualised embeddings.
+    parser = argparse.ArgumentParser()
+    arg = parser.add_argument
+    arg('--input0', '-i0', help='Path to the npz file 0', required=True)
+    arg('--input1', '-i1', help='Path to the npz file 1', required=True)
+    arg('--target', '-t', help='the target word', required=True)
+    arg('--out', '-o', help='Output path for the PCA plot', action="store_true")
+    args = parser.parse_args()
 
-    Usage:
-        pca.py <npz1> <npz2> <target> <out>
-
-    Arguments:
-        <npz1> = path to npz file containing all embeddings for corpus 1 (for all target words)
-        <npz2> = path to npz file containing all embeddings for corpus 2 (for all target words)
-        <target> = the target word
-        <out> = output path for the PCA plot
-
-    """)
-
-    npz1 = args['<npz1>']
-    npz2 = args['<npz2>']
-    target = args['<target>']
-    out_path = args['<out>']
+    npz1 = args.input0
+    npz2 = args.input1
+    target = args.target
+    if args.out:
+        out_path = args.out
+    else:
+        out_path = args.targer + '.png'
 
     usages1 = np.load(npz1)
     usages2 = np.load(npz2)
@@ -65,7 +62,7 @@ def main():
     for matrix, label, color in zip(usages, labels, colors):
         plt.scatter(matrix[:, 0], matrix[:, 1], c=color, s=20)
 
-    plt.legend(labels, prop={'size': 15})
+    plt.legend(labels, prop={'size': 15}, location="best")
 
     plt.savefig(out_path)
     print('Saved plot to file: {}'.format(out_path))
